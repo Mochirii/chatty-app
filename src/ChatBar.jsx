1ffcac18b293fd0d12077616531d8799f40e8188
Mel Smith <Mel@Mels-MacBook-Air-2.local>
2017-11-29 12:36:19 -0800

    edits made to components and app.jsx so that view correctly loads

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,13 +3,11 @@ import React, {Component} from 'react';
 class ChatBar extends Component {
     constructor(props){
         super(props);
-        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     handleKeyPress = function(event) {
         if(event.key === 'Enter'){
-            console.log(event.target.value) 
-            this.props.newChat(event.target.value);  
+            this.props.onMessage(event.target.value);  
             event.target.value="" 
         }
     }
